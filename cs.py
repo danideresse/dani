0990a4bac3293d687150cd80e6fb275fc8d13b44
Kaleab <danideresse15@gmail.com>
2022-12-29 16:13:50 +0300

    Edited by Kaleab

---
+++ cs.py
@@ -0,0 +1,18 @@
+#def sum():
+    #print("Hello World")
+
+#sum()
+
+x,y,z = "Orange","Banana","Apple"
+
+print(x,y,z)
+
+a = 2
+b = 2.1
+print(type(b))
+print(type(a))
+
+name="kaleab"
+height=1.7
+
+print("my name is {:s} and i am {:f} meters tall".format(name,height))
